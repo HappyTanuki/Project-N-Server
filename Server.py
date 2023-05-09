52f5b6e7fa2920dc7656ee29693e1ac1dfc0ff30
happytanuki <rlaalstn120@gmail.com>
2023-05-09 18:24:51 +0900

    .

--- Server.py
+++ Server.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, status, HTTPException
-import uvicorn, pickle, mariadb, sys
+import uvicorn, pickle, sys, json
 from pydantic import BaseModel
 
 '''
@@ -13,55 +13,27 @@ class User(BaseModel):
     id: str
     passwd: str
 
-users = []
-
-'''
-SERVERADDRESS = ""
-
-try:
-    try:
-        with open("serverAddress", 'r') as f:
-            SERVERADDRESS = f.readline()
-    except EOFError:
-        pass
-    except FileNotFoundError:
-        pass
-    else:
-        print("undefined error")
-    conn = mariadb.connect(
-        user            =                                                           "client",
-        password        = "7E6890CCFA16700AB04A41AAADA4FC6B98CE212D7A5D14F9238D2F5F98D1590C",
-        host            =                                                      SERVERADDRESS,
-        port            =                                                               1397,
-        database        =                                                         "projectn"
-    )
-except mariadb.Error as e:
-    print(f"Error connecting to MariaDB Platform: {e}")
-    sys.exit(1)
-
-cur = conn.cursor()
-
-'''
+users = dict()
 
 @app.post("/login")
 async def login(user: User):
-    if (user in users):
+    user = dict(user)
+    if (user["id"] in users and user["passwd"] == users[user["id"]]):
         return user
     else:
         raise HTTPException(status_code=404, detail="User not found.")
 
-
 @app.post("/register")
 async def login(user: User):
-    if user in users:
+    user = dict(user)
+    if user["id"] in users:
         raise HTTPException(status_code=409, detail="User already registered.")
-    users.append(user)
-    with open("members.dat", 'wb') as f:
-        pickle.dump(users, f)
-    print("registered: " + user)
+    else:
+        users[user["id"]] = user["passwd"]
+        with open("members.dat", 'wb') as f:
+            pickle.dump(users, f)
+        print("registered: " + json.dumps(user))
     return user
-
-
 
 if __name__ == "__main__":
     try:
